28552646a2f6437fa3adc1ed2ff3a6f64f6354a9
naqvitalha <naqvitalha@gmail.com>
2017-11-21 13:45:28 +0530

    type safety added to default

--- src/Default.ts
+++ src/Default.ts
@@ -1,7 +1,7 @@
 import { ObjectUtil } from "./ObjectUtil";
 
 export class Default {
-    public static value<T>(obj: any, defaultValue: T): T {
+    public static value<T>(obj: T | null | undefined, defaultValue: T): T {
         return ObjectUtil.isNullOrUndefined(obj) ? defaultValue : obj;
     }
 }
